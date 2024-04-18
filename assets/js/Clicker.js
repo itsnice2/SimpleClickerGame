f5aefe2358f29c3e0f1aaf999c376800dc07c9c3
itsnice2 <itsnice2@web.de>
2024-04-18 16:02:13 +0200

    20240418

---
+++ assets/js/Clicker.js
@@ -0,0 +1,63 @@
+class Clicker
+{
+    /*
+    autoclickerNumbers  = 0;
+    autoclickerPrice    = 10;
+    autoclickerUpgrade  = 1000;
+    autoclicksPerSecond = 1;
+    */
+
+    constructor(autoclickerNumbers, autoclickerPrice, autoclickerUpgrade, autoclicksPerSecond)
+    {
+        this.autoclickerNumbers = autoclickerNumbers;
+        this.autoclickerPrice = autoclickerPrice;
+        this.autoclickerUpgrade = autoclickerUpgrade;
+        this.autoclicksPerSecond = autoclicksPerSecond;
+    }
+
+    /*
+    MyObject = new Clicker();
+
+    NewObject.prototype = {
+        //
+    }
+    */
+
+    
+    buyClicker()
+    {
+        this.autoclickerNumbers = this.autoclickerNumbers + 1;
+        this.autoclickerPrice = this.autoclickerPrice * 1.5;
+    }
+
+    upgradeClicker()
+    {
+        this.autoclicksPerSecond = this.autoclicksPerSecond + 1;
+        this.autoclickerUpgrade = this.autoclickerUpgrade * 1.8;
+    }
+
+    showClickerNumbers()
+    {
+        return this.autoclickerNumbers;
+    }
+
+    showClickerPrice()
+    {
+        return this.autoclickerPrice;
+    }
+
+    showClickerUpgradeCost()
+    {
+        return this.autoclickerUpgrade;
+    }
+
+    showClicksPerSecond()
+    {
+        return this.autoclicksPerSecond;
+    }
+    
+
+}
+
+//const MegaCklicker = new Clicker(0,1000,100000,100);
+
--- assets/js/simpleclickergame.js
+++ assets/js/simpleclickergame.js
@@ -1,7 +1,19 @@
 
+/*
+MegaClicker = new Clicker(0,1000,100000,100);
+UltraClicker = new Clicker(0,1000,100000,100);
+PlanetaryClicker = new Clicker(0,1000,100000,100);
+SolarSystemClicker = new Clicker(0,1000,100000,100);
+GalaxyClicker = new Clicker(0,1000,100000,100);
+UniverseClicker = new Clicker(0,1000,100000,100);
+*/
+
+
 var klicksimbesitz = 0;
 var anzahlklicks = 0;
 var klicksprosekunde = 0;
+
+var footerisvisible = false;
 
 // Manueller Klick
 var manualklicker = 1;
@@ -15,28 +27,68 @@ var autoklickerpreis = 10;
 var autoklickerupgrade = 1000;
 var autoklickerprosekunde = 1;
 
+var test = 0;
+var MegaCklicker = new Clicker(0,1000,100000,100);
 
-//var test = 10;
+function TestFunction()
+{
+	test += 1;
+	MegaCklicker.buyClicker();
+	document.getElementById('TestLabel').innerText = "Test-Funktion: " + test + " - " + MegaCklicker.showClickerNumbers();
+}
+
+const btn = document.querySelector("");
+btn.addEventListener("click", show_footer);
+
+function show_footer()
+{
+	var showchangelog = document.getElementById('versioninfo');
+
+	if(footerisvisible == true)
+	{
+		footerisvisible = false;
+		showchangelog.style.display = "none";
+	}
+	else
+	{
+		footerisvisible = true;
+		showchangelog.style.display = "block";
+	}
+}
+
 
 function click_click()
 {
+	
 	// Klicks ausgeführt aktualisieren
 	klicksimbesitz = klicksimbesitz + manualklicker;
 	document.getElementById('cklickstospend').innerText = klicksimbesitz.toFixed(2);
-	
+
 	// Klicks insgesamt aktualisieren
 	anzahlklicks = anzahlklicks + manualklicker;
 	document.getElementById('clicklabel').innerHTML = anzahlklicks;
-	/*
-	 * Test wegen Multiplikation
-	test = test * 1.1;
-	alert(test.toFixed(2));
-	*/
 }
 
 function upgrade_click_click()
 {
-	//
+	if(klicksimbesitz - manualklickerupgrade < 0)
+	{
+		//
+	}
+	else
+	{
+		// Klicks im Besitz verkleinern
+		klicksimbesitz = klicksimbesitz - manualklickerupgrade;
+		document.getElementById('cklickstospend').innerHTML = klicksimbesitz.toFixed(2);
+
+		// Upgrade-Kosten
+		manualklickerupgrade = manualklickerupgrade * 1.8;
+		document.getElementById('upgrademanualclickcost').innerText = manualklickerupgrade.toFixed(2);
+
+		// Klicks pro Sekunde ändern
+		manualklicker = manualklicker + 2;
+		document.getElementById('upgrademanualclickcount').innerText = manualklicker;
+	}
 }
 
 function buy_autoclicker()
@@ -99,11 +151,11 @@ function upgrade_autoclicker()
 
 // Intervall, alle 1000ms ausführen
 var intervalId = window.setInterval(function(){
-  anzahlklicks = anzahlklicks + klicksprosekunde;
-  document.getElementById('clicklabel').innerHTML = anzahlklicks;
-  klicksprosekunde = (autoklickerprosekunde * autoklicker);
-  document.getElementById('clickspersecond').innerHTML = klicksprosekunde;
-  klicksimbesitz = klicksimbesitz + klicksprosekunde;
-  document.getElementById('cklickstospend').innerHTML = klicksimbesitz.toFixed(2);
-  
-}, 1000);
+	anzahlklicks = anzahlklicks + (klicksprosekunde / 10);
+	document.getElementById('clicklabel').innerHTML = anzahlklicks.toFixed(0);
+	klicksprosekunde = autoklickerprosekunde * autoklicker;
+	document.getElementById('clickspersecond').innerHTML = klicksprosekunde;
+	klicksimbesitz = klicksimbesitz + (klicksprosekunde / 10);
+	document.getElementById('cklickstospend').innerHTML = klicksimbesitz.toFixed(2);
+}, 100);
+
